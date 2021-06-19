44c56111f22dcdd8c41683b9c557f8b6bc07dbc8
Ibaqu <aquib@wso2.com>
2021-06-19 13:59:35 +0530

    strings.py : About strings

---
+++ HelloWorld/strings.py
@@ -0,0 +1,17 @@
+print("This uses double quotes")
+print('This uses single quotes')
+
+print('We can include "double quotes" inside a string')
+print("We can also include 'single quotes' inside a string")
+
+print("String concatenation " + "works like this")
+
+
+# Store values in variable and print variable content
+name = "Aquib"
+print("Hello" + name)
+
+
+# Get input from user
+name = input("Enter your name : ")
+print("Hello : " + name)
