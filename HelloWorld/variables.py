fd3444203ba3459a94a2ca1d199a8791fa66df96
Ibaqu <aquib@wso2.com>
2021-06-24 17:36:37 +0530

    About Data types
    - Automatic type conversion doesn't exist in Python

---
+++ HelloWorld/variables.py
@@ -0,0 +1,16 @@
+age = 24
+
+print(age)
+print(type(age))  # prints type
+print("Aquib is " + age + "years old");  # String concat with int value not allowed
+
+# you can rebind different values to the variable
+
+age = "24 years"
+
+print(age)
+print(type(age))
+print("Aquib is " + age + "years old")
+
+age_in_words = "2 years"
+
