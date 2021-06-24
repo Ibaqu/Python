d1cd547d9d2c40193c61a136a2a6bc8972f4942a
Ibaqu <aquib@wso2.com>
2021-06-24 19:03:52 +0530

    Add strings2.py

---
+++ HelloWorld/strings2.py
@@ -0,0 +1,29 @@
+parrot = "Norwegian Blue"
+
+print(parrot)
+
+print(parrot[3]) # Index 3 'w'
+
+
+# Mini Challenge
+
+print("\n" + parrot[3] +
+      "\n" + parrot[4] +
+      "\n" + parrot[9] +
+      "\n" + parrot[3] +
+      "\n" + parrot[6] +
+      "\n" + parrot[0])     # Prints we win
+
+
+# Negative indexing
+print()
+print(parrot[-1]) # End of string 'e'
+
+
+print()
+print("\n" + parrot[3 - 14] +
+      "\n" + parrot[4 - 14] +
+      "\n" + parrot[9 - 14] +
+      "\n" + parrot[3 - 14] +
+      "\n" + parrot[6 - 14] +
+      "\n" + parrot[0 - 14])
